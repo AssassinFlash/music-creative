b69cd31cef18f9b0c5fb2dace87b4fd1e70a8ba6
AssassinFlash <857558227@qq.com>
2021-09-01 11:40:03 +0800

    改善了Loading插件

--- src/components/Loading/LottieLoading.vue
+++ src/components/Loading/LottieLoading.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
-    <lottie-player
-      src="https://assets5.lottiefiles.com/packages/lf20_pMMQPe.json"
+    <lottie
+      :options="defaultOptions"
       speed="2"
       style="width: 7.5rem; height: 100vh;"
       loop
       autoplay>
-    </lottie-player>
+    </lottie>
   </div>
 </template>
 
 <script>
+import lottie from 'vue-lottie'
+import * as animationData from '@/assets/img/9811-loading.json'
+
 export default {
-  name: 'LottieLoading'
+  name: 'LottieLoading',
+  components: {
+    lottie
+  },
+  data: function () {
+    return {
+      defaultOptions: { animationData: animationData.default }
+    }
+  }
 }
 </script>
 
